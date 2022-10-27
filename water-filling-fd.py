84afac45aae45492e0647bc904efed30e298a100
lurbano <lurbano@earthsciweb.org>
2022-10-27 14:44:04 -0500

    water filling fd

---
+++ water-filling-fd.py
@@ -0,0 +1,32 @@
+import numpy as np 
+import time 
+from ezGraph import *
+
+# Finite Difference Model
+
+# PARAMETERS
+dt = 1.
+nsteps = 20
+
+r = 2.25    # radius (cm)
+Q = 5       # Volume inflow rate: (cubic cm / s)
+h = 0       # Initial height (cm)
+
+# GRAPH
+graph = ezGraph(xmax=30, ymin=0, ymax=10, xLabel="Time (s)", yLabel="Height (cm)")
+
+
+# TIME LOOP
+for t in range(nsteps):
+    modelTime = t * dt
+
+    dh = Q * dt / (np.pi * r**2)    # find the change in height
+    h = h + dh                      # update height
+    
+    print(modelTime, h)
+    graph.add(modelTime, h)
+    graph.wait(0.1)
+
+# DRAW GRAPH
+graph.keepOpen()
+    
