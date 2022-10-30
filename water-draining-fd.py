89bddf5c588dac8b359fab3296dc4a45392b1d6a
lurbano <lurbano@earthsciweb.org>
2022-10-30 13:25:00 -0500

    latex docs

--- water-draining-fd.py
+++ water-draining-fd.py
@@ -5,15 +5,17 @@ from ezGraph import *
 # Finite Difference Model
 
 # PARAMETERS
-dt = 1.
-nsteps = 20
+dt = 1
+nsteps = 100
 
 r = 2.25    # radius (cm)
-Q = 5       # Volume inflow rate: (cubic cm / s)
-h = 0       # Initial height (cm)
+Q = 0       # Volume inflow rate (dV/dt): (cubic cm / s)
+h = 50       # Initial height (cm)
+k = 1.0     # outflow rate constant
 
 # GRAPH
-graph = ezGraph(xmax=30, ymin=0, ymax=10, xLabel="Time (s)", yLabel="Height (cm)")
+graph = ezGraph(xmax=100, 
+                xLabel="Time (s)", yLabel="Height (cm)")
 graph.add(0, h)             # add initial values
 
 
@@ -21,8 +23,14 @@ graph.add(0, h)             # add initial values
 for t in range(1, nsteps):
     modelTime = t * dt
 
+    # Filling
     dh = Q * dt / (np.pi * r**2)    # find the change in height
     h = h + dh                      # update height
+
+    # Draining
+    dVdt = -k * h
+    dh = dVdt * dt / (np.pi * r**2)
+    h = h + dh
     
     print(modelTime, h)
     graph.add(modelTime, h)
